1f429bae9cae3ef9cdd9e6b3068a975bc4a2474e
XuqiuyeLi <xl1029@nyu.edu>
2017-11-19 17:49:08 -0500

    Provided useful classes

--- src/ch04/queues/SyncArrayBoundedQueue.java
+++ src/ch04/queues/SyncArrayBoundedQueue.java
@@ -1,34 +1,36 @@
 //---------------------------------------------------------------------------
-// ArrayBoundedQueue.java        by Dale/Joyce/Weems                Chapter 4
+// SyncArrayBoundedQueue.java     by Dale/Joyce/Weems               Chapter 4
 //
-// Implements QueueInterface with an array to hold the queue elements.
+// Implements BoundedQueueInterface with an array to hold the queue elements.
+// Operations are synchronized to allow concurrent access.
 //
 // Two constructors are provided: one that creates a queue of a default
 // capacity and one that allows the calling program to specify the capacity.
 //---------------------------------------------------------------------------
+
 package ch04.queues;
 
-public class ArrayBoundedQueue<T> implements QueueInterface<T>
+public class SyncArrayBoundedQueue<T> implements QueueInterface<T> 
 {
   protected final int DEFCAP = 100; // default capacity
   protected T[] elements;           // array that holds queue elements
-  protected int numElements = 0;    // number of elements in this queue
+  protected int numElements = 0;    // number of elements in the queue
   protected int front = 0;          // index of front of queue
   protected int rear;               // index of rear of queue
 
-  public ArrayBoundedQueue() 
+  public SyncArrayBoundedQueue() 
   {
     elements = (T[]) new Object[DEFCAP];
     rear = DEFCAP - 1;
   }
 
-  public ArrayBoundedQueue(int maxSize) 
+  public SyncArrayBoundedQueue(int maxSize) 
   {
     elements = (T[]) new Object[maxSize];
     rear = maxSize - 1;
   }
 
-  public void enqueue(T element)
+  public synchronized void enqueue(T element)
   // Throws QueueOverflowException if this queue is full;
   // otherwise, adds element to the rear of this queue.
   {  
@@ -42,10 +44,10 @@ public class ArrayBoundedQueue<T> implements QueueInterface<T>
     }
   }
 
-  public T dequeue()
+  public synchronized T dequeue()
   // Throws QueueUnderflowException if this queue is empty;
   // otherwise, removes front element from this queue and returns it.
-  {   
+  {       
     if (isEmpty())
       throw new QueueUnderflowException("Dequeue attempted on empty queue.");
     else
@@ -58,22 +60,21 @@ public class ArrayBoundedQueue<T> implements QueueInterface<T>
     }
   }
 
-  public boolean isEmpty()
-  // Returns true if this queue is empty; otherwise, returns false.
+  public synchronized boolean isEmpty()
+  // Returns true if this queue is empty; otherwise, returns false
   {              
     return (numElements == 0);
   }
 
-  public boolean isFull()
+  public synchronized boolean isFull()
   // Returns true if this queue is full; otherwise, returns false.
   {              
     return (numElements == elements.length);
   }
-  
-  public int size()
+
+  public synchronized int size()
   // Returns the number of elements in this queue.
   {
     return numElements;
   }
-  
 }
